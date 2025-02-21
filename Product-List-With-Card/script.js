c54d9fd300200e7b9d7b0700de4ad80ac8e4e2b9
twiztss <paritch.kae@gmail.com>
2025-02-21 14:56:35 +0700

    Add function to add product to the cart.

--- Product-List-With-Card/script.js
+++ Product-List-With-Card/script.js
@@ -56,19 +56,77 @@ let MenuList = [
 ]
 
 const btn = document.getElementsByClassName("add-cart-btn")
+const cartContent = document.querySelector(".cart-content")
+const emptyCart = document.querySelector(".empty-cart")
 
 let total = MenuList.reduce((prev, cur) => prev + cur.price * cur.amount, 0)
 
 for (let i = 1; i < btn.length; i++) {
    btn[i].addEventListener("click", (e) => {
+      console.log(cartContent.childNodes)
       const { name } = e.target
       MenuList = MenuList.map((item) => {
          if (name == item.category) {
                newItem = {...item, amount : item.amount + 1}
+               const cartList = createCartList(item)
+
+               isEmpty()
+
+               cartContent.appendChild(cartList)
                return newItem
          } else {
             return item
          }
       })
    })
+}
+
+const createCartList = (item) => {
+   const cartList = createElementWithClass("div", "cart-list")
+
+   cartList.setAttribute("name", item.name)
+
+   const listText = createElementWithClass("div", "cart-list-text")
+   const listStat = createElementWithClass("div", "cart-list-stat")
+
+   const listTitle = createElementWithClass("p", "")
+   const listAmount = createElementWithClass("p", "cart-list-amount")
+   const listPrice = createElementWithClass("p", "cart-list-price")
+   const listTotal = createElementWithClass("p", "cart-list-total")
+
+   listTitle.textContent = item.name
+   listAmount.textContent = "x" + item.amount
+   listPrice.textContent = "@" + (item.price)
+   listTotal.textContent = "$" + (item.price * item.amount)
+
+   listStat.appendChild(listAmount)
+   listStat.appendChild(listPrice)
+   listStat.appendChild(listTotal)
+
+   listText.appendChild(listTitle)
+   listText.appendChild(listStat)
+
+   cartList.appendChild(listText)
+
+   return cartList
+}
+
+const createElementWithClass = (type="div", className) => {
+   const element = document.createElement(type)
+
+   if (className) {
+      element.classList.add(className)
+      return element
+   } else {
+      return element
+   }
+
+}
+
+const isEmpty = () => {
+   if (cartContent.childElementCount < 0) {
+      emptyCart.style.display = "flex"
+   } else {
+      emptyCart.style.display = "none"
+   }
 }
